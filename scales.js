3f7c12a3c259a98a635aee131e789c40d4389fe2
carlos cordoba <carloscba1@hotmail.com>
2017-05-28 09:52:37 -0300

    - scales

--- scales.js
+++ scales.js
@@ -1,5 +1,5 @@
 
-dataTile = ['ELECTROINGENIERIA SA','TARJETAS REGIONALES SA','FIDEICOMISO EDISUR 1','GRUPO ELING SA','CEMENTOS AVELLANEDA S.A.','GAMA  S.A.','HUAWEI TECH INVESTMENT CO LTD','ASOC. GREM. EMPL. DE COMERCIO','MASSALIN PARTICULARES SRL','PIRELLI NEUMATICOS SAIC','EDISUR S.A.'];
+dataTitle = ['ELECTROINGENIERIA SA','TARJETAS REGIONALES SA','FIDEICOMISO EDISUR 1','GRUPO ELING SA','CEMENTOS AVELLANEDA S.A.','GAMA  S.A.','HUAWEI TECH INVESTMENT CO LTD','ASOC. GREM. EMPL. DE COMERCIO','MASSALIN PARTICULARES SRL','PIRELLI NEUMATICOS SAIC','EDISUR S.A.'];
 dataAmount = [8255187,7051751,6264031,5044982,4322514,3739615,3706458,3551534,3528470,3428048,3212456];
 
 let width = window.innerWidth;
@@ -9,7 +9,7 @@ let min = d3.min(dataAmount);
 let max = d3.max(dataAmount);
 
 const margin = {
-    top :0,
+    top :10,
     left: 80,
     bottom: 50,
     right: 0,
@@ -21,6 +21,7 @@ let scalesSvg = d3.select('#scales').append('svg')
 
 let chartGroup = scalesSvg.append('g').attr('transform','translate('+margin.left+','+margin.top+')')
 
+//-----Y-------
 let y = d3.scaleLinear().domain([min, max])
                         .range([height, 0]);
 
@@ -31,19 +32,18 @@ let yAxis = d3.axisLeft(y)
                 
 chartGroup.append('g').attr('class', 'axis y').call(yAxis)
 
-let x = d3.scaleLinear().domain([0, 10])
+//-----X-------
+let x = d3.scalePoint().domain(dataTitle)
                         .range([0, width]);
 
-let xAxis = d3.axisBottom(x)
+let xAxis = d3.axisTop(x)
                 .ticks(10)
                 .tickPadding(5)
                 .tickSize(5);
 
-chartGroup.append('g').attr('class', 'axis X').call(xAxis).attr('transform', 'translate(0, '+ height +')');
+chartGroup.append('g').attr('class', 'axis X').call(xAxis);
 
-
-
-//
+//-----Graph-------
 let widthItem = (width / dataAmount.length);
 
 chartGroup.selectAll('rect')
